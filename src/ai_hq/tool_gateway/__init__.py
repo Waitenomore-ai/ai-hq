9d8144dd880c4d81e4e1d6586f051faae6649a2d
Waitenomore-ai <campcreativerse@gmail.com>
2026-09-03 23:38:04 +0100

    feat: export Tool Gateway contracts

---
+++ src/ai_hq/tool_gateway/__init__.py
@@ -0,0 +1,15 @@
+from ai_hq.tool_gateway.contracts import (
+    ToolAdapter,
+    ToolOutcome,
+    ToolOutcomeState,
+    ToolRequest,
+)
+from ai_hq.tool_gateway.registry import ToolRegistry
+
+__all__ = [
+    "ToolAdapter",
+    "ToolOutcome",
+    "ToolOutcomeState",
+    "ToolRegistry",
+    "ToolRequest",
+]
