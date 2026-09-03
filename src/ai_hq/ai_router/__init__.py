ffa5cd4b8ec9435c3e218734b2abe65a0909c161
Waitenomore-ai <campcreativerse@gmail.com>
2026-09-03 18:34:19 +0100

    feat: add AI Router package

---
+++ src/ai_hq/ai_router/__init__.py
@@ -0,0 +1,11 @@
+from ai_hq.ai_router.registry import ModelEndpoint, ModelRegistry, ProviderKind
+from ai_hq.ai_router.router import CapabilityClass, ModelRouter, RouteDecision
+
+__all__ = [
+    "CapabilityClass",
+    "ModelEndpoint",
+    "ModelRegistry",
+    "ModelRouter",
+    "ProviderKind",
+    "RouteDecision",
+]
