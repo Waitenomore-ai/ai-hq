945d98f1402976053f61748dbb2ac86136e1639c
Waitenomore-ai <campcreativerse@gmail.com>
2026-09-03 17:02:45 +0100

    feat: export Knowledge Core primitives

---
+++ src/ai_hq/knowledge/__init__.py
@@ -0,0 +1,17 @@
+from ai_hq.knowledge.models import (
+    KnowledgeMemory,
+    MemoryCategory,
+    MemorySensitivity,
+    MemoryVisibility,
+    VerificationState,
+)
+from ai_hq.knowledge.service import KnowledgeService
+
+__all__ = [
+    "KnowledgeMemory",
+    "KnowledgeService",
+    "MemoryCategory",
+    "MemorySensitivity",
+    "MemoryVisibility",
+    "VerificationState",
+]
