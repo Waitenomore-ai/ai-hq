9b40cb316cf6a472c1ae12986c2260756a337d01
Waitenomore-ai <campcreativerse@gmail.com>
2026-09-03 20:08:20 +0100

    feat: add host helper package

---
+++ src/ai_hq/host_helper/__init__.py
@@ -0,0 +1,15 @@
+from ai_hq.host_helper.contracts import (
+    HelperRequest,
+    HelperResponse,
+    HostAllowLists,
+    HostCapability,
+    validate_request,
+)
+
+__all__ = [
+    "HelperRequest",
+    "HelperResponse",
+    "HostAllowLists",
+    "HostCapability",
+    "validate_request",
+]
