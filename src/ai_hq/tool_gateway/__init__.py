f05d3c9f8c851954d4cc514ad3a2bbb21c13877f
Waitenomore-ai <campcreativerse@gmail.com>
2026-09-03 23:49:09 +0100

    feat: export complete Tool Gateway boundary

--- src/ai_hq/tool_gateway/__init__.py
+++ src/ai_hq/tool_gateway/__init__.py
@@ -1,13 +1,19 @@
 from ai_hq.tool_gateway.contracts import (
     ToolAdapter,
+    ToolAdapterError,
     ToolOutcome,
     ToolOutcomeState,
     ToolRequest,
 )
+from ai_hq.tool_gateway.host_adapter import HostHelperAdapter
 from ai_hq.tool_gateway.registry import ToolRegistry
+from ai_hq.tool_gateway.service import ToolGateway
 
 __all__ = [
+    "HostHelperAdapter",
     "ToolAdapter",
+    "ToolAdapterError",
+    "ToolGateway",
     "ToolOutcome",
     "ToolOutcomeState",
     "ToolRegistry",
